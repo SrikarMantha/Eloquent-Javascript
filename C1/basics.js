a5da11ae63e267aa1f7f6e82613a4df0259260cf
SrikarMantha <srikarmantha26@gmail.com>
2018-06-30 19:56:33 +0530

    Add automatic type conversion

--- C1/basics.js
+++ C1/basics.js
@@ -21,3 +21,10 @@ console.log(true && false)
 // || (Logical OR)
 console.log(false || true)
 console.log(false || false)
+
+// AUTOMATIC TYPE CONVERSION
+console.log(8 * null) // null becomes 0
+console.log("5" - 1) // "5" is converted to number
+console.log("5" + 1) // Occurrence of string concatenation
+console.log("five" * 2) // Strings can't be multipled. So NaN occurs
+console.log(false == 0) // Converts one value to other value type
