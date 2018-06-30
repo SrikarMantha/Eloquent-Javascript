5c692e96fe196f1a1cad6d7159d800303cf51b31
SrikarMantha <srikarmantha26@gmail.com>
2018-06-30 19:45:58 +0530

    Add logical operators

---
+++ C1/basics.js
@@ -0,0 +1,23 @@
+ // ${} converts content in ` ` into a string (STRINGS)
+console.log(`Half of 100 is ${100/2}`);
+
+// Get the type of value (UNARY OPERATORS)
+console.log(typeof 6)
+console.log(typeof "6")
+
+// COMPARISON
+console.log(4>1)
+console.log(4<1)
+
+// Comparing strings (COMPARISON)
+console.log("Itchy" != "Scratchy")
+console.log("Apple" == "Orange")
+
+// LOGICAL OPERATORS
+// && (Logical AND)
+console.log(true && true)
+console.log(true && false)
+
+// || (Logical OR)
+console.log(false || true)
+console.log(false || false)
