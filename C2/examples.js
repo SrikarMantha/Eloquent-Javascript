336c45347bfb42ed309fd91b2cd56f13e9e76cfc
SrikarMantha <srikarmantha26@gmail.com>
2018-07-01 17:31:15 +0530

    Add while and do loops

--- C2/examples.js
+++ C2/examples.js
@@ -48,3 +48,24 @@ if (num < 10) {
 } else {
   console.log("Large");
 }
+
+// WHILE AND DO LOOPS
+let number = 0;
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+}
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
+
+let yourName;
+do {
+  yourName = prompt("Who are you?");
+} while (!yourName);
+console.log(yourName);
