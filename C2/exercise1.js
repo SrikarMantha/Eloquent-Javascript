13e8bee7bffb0e90f68eeb8b584c4492d0d40399
SrikarMantha <srikarmantha26@gmail.com>
2018-07-01 22:53:36 +0530

    Add exercise 1

---
+++ C2/exercise1.js
@@ -0,0 +1,18 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+// CODE
+var a = " ";
+for(i=0; i<7 ; i++) {
+  a+= "#";
+  console.log(a);
+}
