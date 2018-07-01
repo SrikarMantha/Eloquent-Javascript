a3134e574f1698c0ebe83e95644863672e951aa8
SrikarMantha <srikarmantha26@gmail.com>
2018-07-01 12:08:49 +0530

    Add variables

---
+++ C2/variables.js
@@ -0,0 +1,21 @@
+// Defining a variable (BINDING)
+let ten = 10;
+console.log(ten * ten);
+
+// Assigning a new value
+let mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood);
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+
+// Defining multiple bindings
+let one = 1 , two = 2;
+console.log(one + two);
+
+// const (points at the same value for as long as it lives)
+const greeting = "hello";
+console.log(greeting);
