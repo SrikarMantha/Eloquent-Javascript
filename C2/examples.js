965d77c9d3314a72b12f2c88b046066fe0e573ab
SrikarMantha <srikarmantha26@gmail.com>
2018-07-01 14:53:28 +0530

    Add if else conditionals

--- C2/examples.js
+++ C2/examples.js
@@ -19,3 +19,32 @@ console.log(one + two);
 // const (points at the same value for as long as it lives)
 const greeting = "hello";
 console.log(greeting);
+
+let x = 30;
+console.log("the value of x is", x);
+
+console.log(Math.max(22, 8)); // Math.max function returns greatest value
+console.log(Math.min(2, 4) + 100);
+
+//CONTROL FLOW
+let theNumber = Number(prompt("Pick a number")); // Number converts the given string into number
+console.log("Your number is the square root of " + theNumber * theNumber);
+
+// CONDITIONAL EXECUTION
+let theNumber = Number(prompt("Pick a number"));
+if (Number.isNaN(theNumber)) {
+  console.log("Enter a Number")
+}
+else{
+  console.log("Your number is the square root of " + theNumber * theNumber);
+}
+
+// else if
+let num = Number(prompt("Pick a number"));
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
