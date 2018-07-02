4d749cc2f940a485beab11f1960860109780b574
SrikarMantha <srikarmantha26@gmail.com>
2018-07-02 20:01:16 +0530

    Add optional arguments

--- C3/examples.js
+++ C3/examples.js
@@ -37,3 +37,34 @@ console.log("The future says:", future());
 function future() {
   return "You'll never have flying cars";
 }
+
+// THE CALL STACK
+function greet(who) {
+  console.log("Hello " + who);
+}
+greet("Harry");
+console.log("Bye");
+
+// OPTIONAL ARGUMENTS
+// In the following example we'll define square with only one parameter. Yet when we'll call it with three, the language doesn’t complain. It ignores the extra arguments and computes the square of the first one.
+function square(x) { return x * x; }
+console.log(square(8 , true, "hedgehog"));
+
+
+function minus(a, b) {
+  if (b === undefined) return -a;
+  else return a - b;
+}
+console.log(minus(10));
+console.log(minus(10, 5));
+
+// If you write an = operator after a parameter, followed by an expression, the value of that expression will replace the argument when it is not given.
+function power(base, exponent = 2) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+console.log(power(4));
+console.log(power(2, 6));
