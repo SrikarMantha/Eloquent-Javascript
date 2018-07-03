818305a64b719fbe398cc5216e101da07c0181e8
SrikarMantha <srikarmantha26@gmail.com>
2018-07-03 20:56:52 +0530

    Add methods

--- C4/examples.js
+++ C4/examples.js
@@ -3,3 +3,16 @@ let listOfNumbers = [2, 3, 5, 7, 11];
 console.log(listOfNumbers);
 console.log(listOfNumbers[2]);
 listOfNumbers.length;
+
+// METHODS
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+// Methods to manipulate arrays
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
