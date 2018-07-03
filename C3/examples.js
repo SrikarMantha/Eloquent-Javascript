a7a4ada4a1bb83401088baaca7276823bfa7da11
SrikarMantha <srikarmantha26@gmail.com>
2018-07-03 14:08:52 +0530

    Add growing functions

--- C3/examples.js
+++ C3/examples.js
@@ -85,3 +85,18 @@ function power(base, exponent) {
   }
 }
 console.log(power(2, 3));
+
+// GROWING FUNCTIONS
+function printFarmInventory(cows, chickens) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = "0" + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chickens);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11);
