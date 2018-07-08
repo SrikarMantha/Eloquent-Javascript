76e47a08dcb7b25e539ef4b4a0b32ef372f4da43
SrikarMantha <srikarmantha26@gmail.com>
2018-07-08 17:06:18 +0530

    Add nested scope

--- C3/examples.js
+++ C3/examples.js
@@ -32,6 +32,25 @@ const halve = function(n) {
 let n = 10;
 console.log(halve(100)); // The code inside the halve function refers to n, it is seeing its own n, not the global n.
 
+// NESTED SCOPE
+
+const hummus = function(factor) {
+  const ingredient = function(amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+hummus(1);
+
 // DECLARATION NOTATION
 console.log("The future says:", future());
 function future() {
