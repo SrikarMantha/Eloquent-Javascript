03d51476f4ff66732122370329b6c71d3fe8b5e7
SrikarMantha <srikarmantha26@gmail.com>
2018-07-13 21:45:31 +0530

    Add prototypes

--- C6/examples.js
+++ C6/examples.js
@@ -13,3 +13,30 @@ let whiteRabbit = {type: "white", speak};
 let hungryRabbit = {type: "hungry", speak};
 whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
 hungryRabbit.speak("I could use a carrot right now.");
+
+// If you want to pass this explicitly, you can use a function’s call method, which takes the this value as its first argument and treats further arguments as normal parameters
+speak.call(hungryRabbit, "Burp!")
+speak.call(whiteRabbit, "Destroy them")
+
+//PROTOTYPES
+let empty = {};
+console.log(empty.toString);
+console.log(empty.toString());
+
+// Object.getPrototypeOf returns the prototype of an object
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype));
+
+// Functions derive from Function.prototype, and arrays derive from Array.prototype
+console.log(Object.getPrototypeOf(Math.max) == Function.prototype);
+console.log(Object.getPrototypeOf([]) == Array.prototype);
+
+// You can use Object.create to create an object with a specific prototype
+let protoRabbit = {
+  speak(line) {   // A property like speak(line) in an object expression is a shorthand way of defining a method. It creates a property called speak and gives it a function as its value
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEE!");
