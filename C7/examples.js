2894c707c3ab4ec8209df3f789a4392874521feb
SrikarMantha <srikarmantha26@gmail.com>
2018-07-24 00:00:58 +0530

    Add random direction function

--- C7/examples.js
+++ C7/examples.js
@@ -59,3 +59,25 @@ let next = first.move("Alice's House");
 console.log(next.place);
 console.log(next.parcels);
 console.log(first.place);
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+// RANDOM DIRECTION
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
