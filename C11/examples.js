b0a33d5b5d9ed61fe19e8179915f23ac3c4b0788
SrikarMantha <srikarmantha26@gmail.com>
2018-08-01 16:56:57 +0530

    Add collections of promises

--- C11/examples.js
+++ C11/examples.js
@@ -20,3 +20,38 @@ new Promise((_, reject) => reject(new Error("Fail")))
     return "nothing";
   })
   .then(value => console.log("Handler 2", value));
+
+// NETWORKS ARE HARD
+class Timeout extends Error {}
+
+function request(nest, target, type, content) {
+  return new Promise((resolve, reject) => {
+    let done = false;
+    function attempt(n) {
+      nest.send(target, type, content, (failed, value) => {
+        done = true;
+        if (failed) reject(failed);
+        else resolve(value);
+      });
+      setTimeout(() => {
+        if (done) return;
+        else if (n < 3) attempt(n + 1);
+        else reject(new Timeout("Timed out"));
+      }, 250);
+    }
+    attempt(1);
+  });
+}
+
+// COLLECTIONS OF PROMISES
+requestType("ping", () => "pong");
+
+function availableNeighbors(nest) {
+  let requests = nest.neighbors.map(neighbor => {
+    return request(nest, neighbor, "ping")
+      .then(() => true, () => false);
+  });
+  return Promise.all(requests).then(result => {
+    return nest.neighbors.filter((_, i) => result[i]);
+  });
+}
