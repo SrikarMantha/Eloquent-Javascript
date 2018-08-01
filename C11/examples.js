54dcddf40c394ba94c5e769b1c832544ae148e52
SrikarMantha <srikarmantha26@gmail.com>
2018-08-01 13:05:09 +0530

    Add failure

--- C11/examples.js
+++ C11/examples.js
@@ -11,3 +11,12 @@ function storage(nest, name) {
   });
 }
 storage(bigOak, "enemies").then(value => console.log("Got", value));
+
+// FAILURE
+new Promise((_, reject) => reject(new Error("Fail")))
+  .then(value => console.log("Handler 1"))
+  .catch(reason => {
+    console.log("Caught failure " + reason);
+    return "nothing";
+  })
+  .then(value => console.log("Handler 2", value));
