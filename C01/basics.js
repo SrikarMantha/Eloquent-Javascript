d19ef11aa4c15c94c54471145c4e0cd1bdce56ce
SrikarMantha <srikarmantha26@gmail.com>
2018-08-14 23:49:04 +0530

    Renamed

---
+++ C01/basics.js
@@ -0,0 +1,35 @@
+ // ${} converts content in ` ` into a string (STRINGS)
+console.log(`Half of 100 is ${100/2}`);
+
+// Get the type of value (UNARY OPERATORS)
+console.log(typeof 6)
+console.log(typeof "6")
+
+// COMPARISON
+console.log(4>1)
+console.log(4<1)
+
+// Comparing strings (COMPARISON)
+console.log("Itchy" != "Scratchy")
+console.log("Apple" == "Orange")
+
+// LOGICAL OPERATORS
+// && (Logical AND)
+console.log(true && true)
+console.log(true && false)
+
+// || (Logical OR)
+console.log(false || true)
+console.log(false || false)
+
+// AUTOMATIC TYPE CONVERSION
+console.log(8 * null) // null becomes 0
+console.log("5" - 1) // "5" is converted to number
+console.log("5" + 1) // Occurrence of string concatenation
+console.log("five" * 2) // Strings can't be multipled. So NaN occurs
+console.log(false == 0) // Converts one value to other value type
+
+// SHORT CIRCUITING OF LOGICAL OPERATORS
+console.log(null || "user")
+console.log("Agnes" || "user")
+//The || operator will return the value to its left when that can be converted to true and will return the value on its right otherwise. 
